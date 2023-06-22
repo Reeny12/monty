3b2fba420c0620a7904db931e15d510c0af5e284
Reeny12 <lebotsamaureen@gmail.com>
2023-06-22 12:32:03 -0700

    Stacks and Queues

--- pchar.c
+++ pchar.c
@@ -10,6 +10,9 @@ void f_pchar(stack_t **head, unsigned int counter)
 	stack_t *h;
 
 	h = *head;
+
+	/*error statement*/
+
 	if (!h)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", counter);
@@ -18,6 +21,8 @@ void f_pchar(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
+
+	/*error statement*/
 	if (h->n > 127 || h->n < 0)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", counter);
@@ -26,5 +31,8 @@ void f_pchar(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
+
+	/*printf function*/
+
 	printf("%c\n", h->n);
 }
